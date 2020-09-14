900b043443afa3a7cc793d3ce992c7e828bd030a
Dwin <66736941+Dwin17@users.noreply.github.com>
2020-09-13 23:08:33 -0700

    Update bento.h

--- bento.h
+++ bento.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+/* Copyright 2020 Dennis Nguyen <u/nguyedt>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
