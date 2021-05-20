21aa22a2eb4ca969ed43952902aa3cc6408b3d70
c0qu1 <ald52sunshine@gmail.com>
2021-05-20 00:00:10 -0400

    Changed the vendorId to a unique value for VIA to properly recognize this version of the Bento Macropad. Added a copy of the config.h file to the keymaps/via directory to have specific configuration for the VIA version of the firmware. For example, to minimize the size of the firmware the RGBLEDs effects were reduced to 2. Also added support for RGBLIGHT_LAYERS to identify each layer with a different color, this still works with the RGB effects because the LED indicating the layer is just the middle one. NOTE: The key for switching layers should not be modify using VIA, because there is no way to put it back using VIA. This will require a reflash using QMK Toolbox. The keymap.c now includes functionality in all four layers. The rotary encoder was per layer functionality too. To switch layers, the 3rd key on the 2nd row below the rotary encoder will be used, tapping the key will switch from _MEDIA > _BROWSER > _MACRO > _RGBLIGHTS > _MEDIA > _BROWSER ...

---
+++ keymaps/via/config.h
@@ -0,0 +1,82 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+#define QMK_ESC_OUTPUT F1 // usually COL
+#define QMK_ESC_INPUT D5 // usually ROW
+#define QMK_LED B0
+#define QMK_SPEAKER C6
+#define AUDIO_PIN C6
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x6E67  // 'NG'uyedt
+#define PRODUCT_ID      0x2133
+#define DEVICE_VER      0x0100
+#define MANUFACTURER    nguyedt
+#define PRODUCT         Bento
+#define DESCRIPTION     2x3 Macropad with Rotary Encoder
+
+/* key matrix size */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 3
+
+/* Keyboard Matrix Assignments */
+#define DIRECT_PINS { \
+    { D2, D4, F4 }, \
+    { D7, B1, B3 } \
+}
+
+/* Handwired Pin Layout:
+
+    { D7, B1, D2 }, \
+    { E6, B4, B2 } \
+*/
+
+#define ENCODERS_PAD_A {D1}
+#define ENCODERS_PAD_B {F5}
+#define ENCODER_DIRECTION_FLIP
+
+#define BACKLIGHT_PIN B6
+#define BACKLIGHT_LEVELS 7
+
+#define RGB_DI_PIN D3
+#ifdef RGB_DI_PIN
+  #undef RGBLED_NUM
+  #define RGBLED_NUM 3
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+  #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+  /*== all animations enable ==*/
+  /* #define RGBLIGHT_ANIMATIONS */
+  #define RGBLIGHT_EFFECT_TWINKLE
+  #define RGBLIGHT_EFFECT_KNIGHT
+  #define RGBLIGHT_EFFECT_KNIGHT_LENGTH 2
+  #define RGBLIGHT_LAYERS
+  #define RGBLIGHT_MAX_LAYERS 5
+  #define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -16,65 +16,167 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-  _BASE,
-  _VIA1,
-  _VIA2,
-  _VIA3
+  _MEDIA,
+  _BROWSER,
+  _MACRO,
+  _RGBLIGHTS
+};
+
+enum custom_keycodes {
+  KC_CYCLE_LAYERS = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-        |                  |              | Knob : Vol Up/Dn  |
-        |  Toggle Layer 1  |   Browser Search    |    Press: Mute    |
-        |  Browser Back    |   Browser Refresh   |   Browser Forward |
+        |                  |                    |  Knob : Vol Up/Dn |
+        |      Mail        |         Play       |    Press: Mute    |
+        |      Prev        |         Next       |  Cycle Layers     |
      */
-    [_BASE] = LAYOUT(
-        MO(1), KC_WSCH, KC_MUTE,
-        KC_WBAK , KC_WREF  , KC_WFWD
+    [_MEDIA] = LAYOUT(
+        KC_MAIL, KC_MPLY, KC_MUTE,
+        KC_MPRV, KC_MNXT, KC_CYCLE_LAYERS
     ),
     /*
-        |               |                        | Knob : Page Up/Dn |
-        |               |   Increase Brightness  |     Toggle RGB    |
-        |    RGB Cycle  |   Increase Brightness  |  Hue Cycle        |
+        |                  |                     |  Knob : Desktops  |
+        |       Back       |   Fwd               |    Press: Stop    |
+        |     PrevTab      | NextTab             |   Cycle Layers    |
      */
-    [_VIA1] = LAYOUT(
-        _______, KC_RSFT, RGB_TOG,
-        RGB_MOD, RGB_VAI, RGB_HUI
+    [_BROWSER] = LAYOUT(
+        KC_WBAK, KC_WFWD, KC_WSTP,
+        S(C(KC_TAB)), C(KC_TAB), KC_CYCLE_LAYERS
     ),
     /*
-        |               |               |               |
-        |               |               |               |
-        |               |               |               |
+        |               |                        |  Knob : Windows    |
+        | Slack Status  |    Zoom Toggle Mute    | Transparent        |
+        |  WinScrnSht   |        Task View       |  Cycle Layers      |
      */
-    [_VIA2] = LAYOUT(
-        _______, _______, _______,
-        _______, _______, _______
+    [_MACRO] = LAYOUT(
+        C(S(KC_Y)), A(KC_A), _______,
+        G(S(KC_S)), G(KC_TAB), KC_CYCLE_LAYERS
     ),
     /*
-        |               |               |               |
-        |               |               |               |
-        |               |               |               |
+        |               |                        | Knob : Saturation Up/Dn |
+        | RGB MOD Cycle |   Right Shift          |     Toggle RGB          |
+        |    Hue Cycle  |   Increase Brightness  |  Cycle Layers           |
      */
-    [_VIA3] = LAYOUT(
-        _______, _______, _______,
-        _______, _______, _______
+    [_RGBLIGHTS] = LAYOUT(
+        RGB_MOD, KC_RSFT, RGB_TOG,
+        RGB_HUI, RGB_VAI, KC_CYCLE_LAYERS
     ),
 
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (layer_state_is(0)) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
+const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 0, HSV_WHITE}
+);
+// Light LED 1 in cyan when keyboard layer 1 is active
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_CYAN}
+);
+// Light LED 2 in purple when keyboard layer 2 is active
+const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_PURPLE}
+);
+// Light LED 3 in green when keyboard layer 3 is active
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {1, 1, HSV_RED}
+);
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_layer0_layer,
+    my_layer1_layer,    // Overrides other layers
+    my_layer2_layer,    // Overrides other layers
+    my_layer3_layer    // Overrides other layers
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, layer_state_cmp(state, _MEDIA));
+    return state;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, _BROWSER));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _MACRO));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _RGBLIGHTS));
+    return state;
+}
+
+uint8_t selected_layer = 0;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
+    case KC_CYCLE_LAYERS: //custom macro
+      if (record->event.pressed) {
+        //SEND_STRING("I love QMK with VIA!");
+        selected_layer++;
+        if(selected_layer > 3) { selected_layer = 0;}
+        layer_clear();
+        layer_on(selected_layer);
+      }
+    break;
+  }
+return true;
+}
+
+// RGB config, for changing RGB settings on non-VIA firmwares
+void change_RGB(bool clockwise) {
+    bool shift = get_mods() & MOD_MASK_SHIFT;
+    bool alt = get_mods() & MOD_MASK_ALT;
+    bool ctrl = get_mods() & MOD_MASK_CTRL;
+
+    if (clockwise) {
+        if (alt) {
+            rgblight_increase_hue();
+        } else if (ctrl) {
+            rgblight_increase_val();
+        } else if (shift) {
+            rgblight_increase_sat();
         } else {
-            tap_code(KC_VOLD);
+            rgblight_step();
+        }
+
+  } else {
+      if (alt) {
+            rgblight_decrease_hue();
+        } else if (ctrl) {
+            rgblight_decrease_val();
+        } else if (shift) {
+            rgblight_decrease_sat();
+        } else {
+            rgblight_step_reverse();
         }
     }
-    else if (layer_state_is(1)) {
-        if (clockwise) {
-            tap_code(KC_UP);
-        } else {
-            tap_code(KC_DOWN);
-        }
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    switch (biton32(layer_state))
+    {
+        case _MEDIA:
+            clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
+            break;
+        case _BROWSER:
+            register_code(KC_LCTRL);
+            register_code(KC_LGUI);
+            clockwise ? tap_code(KC_RGHT) : tap_code(KC_LEFT);
+            unregister_code(KC_LGUI);
+            unregister_code(KC_LCTRL);
+            break;
+        case _MACRO:
+            if (!clockwise) { register_code(KC_LSFT); }
+            register_code(KC_LCTRL);
+            register_code(KC_LALT);
+            tap_code(KC_TAB);
+            unregister_code(KC_LCTRL);
+            unregister_code(KC_LALT);
+            if(!clockwise) { unregister_code(KC_LSFT); }
+            break;
+        case _RGBLIGHTS:
+            //clockwise ? tap_code(RGB_SAI) : tap_code(RGB_SAD);
+            change_RGB(clockwise);
+            break;
     }
 }
