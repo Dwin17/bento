129cf8372e3103fe1d035f8b73783eb6aa31ac82
Xironix <salamunkai@gmail.com>
2023-02-06 23:03:46 +0100

    Updated for newer version of QMK

--- config.h
+++ config.h
@@ -17,14 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xCB10
-#define PRODUCT_ID      0x1133
-#define DEVICE_VER      0x0100
-#define MANUFACTURER    nguyedt
-#define PRODUCT         Bento
-#define DESCRIPTION     2x3 Macropad with Rotary Encoder
-
 /* key matrix size */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 3
@@ -55,8 +47,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-  /*== all animations enable ==*/
-  #define RGBLIGHT_ANIMATIONS
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
